8f346fbadada181569966f6dccca6cb55c6dd5e3
hitoshiQQ <127374716+hitoshiQQ@users.noreply.github.com>
2023-04-25 01:11:29 +0600

    Add files via upload

---
+++ practice 19/Task 4/Homework 19.4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+bool isPng(const std::string& filename) {
+    std::ifstream inFile(filename.c_str(), std::ios::binary);
+
+    std::string fileType = filename.substr(filename.length() - 4);
+    if (fileType != ".png" && fileType != ".PNG") return false;
+    char fileNum;
+    std::string fileChar;
+    if (inFile.is_open()) {
+        inFile >> fileNum;
+        for (int i = 0; i < 3; ++i) {
+            char tmp;
+            inFile >> tmp;
+            fileChar += tmp;
+        }
+        inFile.close();
+    }
+    else {
+        std::cout << "File not found." << std::endl;
+    }
+    return (fileNum == -119 && fileChar == "PNG");
+}
+
+int main()
+{
+    std::cout << "Enter the full file name: ";
+    std::string fileName;
+    std::cin >> fileName;
+    std::cout << (isPng(fileName) ? "This is a png file" : "It is not a png file") << std::endl;
+
+    return 0;
+}
+
