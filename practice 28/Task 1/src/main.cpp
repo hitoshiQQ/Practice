b1c5b784b02484de721f0250ad50115cceecf506
hitoshiQQ <127374716+hitoshiQQ@users.noreply.github.com>
2023-06-16 14:07:11 +0600

    Update main.cpp

--- practice 28/Task 1/src/main.cpp
+++ practice 28/Task 1/src/main.cpp
@@ -3,14 +3,14 @@
 #include <mutex>
 #include <cmath>
 #include <ctime>
-#include "Result.h"
+#include "Threading.h"
 #include "Swimmers.h"
 
 std::mutex access;
 
 int main() {
 	std::vector<Swimmers*> swimmers;
-	Result result;
+	Threading threading;
 
 	for (int j = 1; j <= 6; ++j) {
 		swimmers.push_back(new Swimmers(j, Enters::enterPositionDouble("Enter swimmer speed #" + std::to_string(j) + ": ")));
@@ -18,11 +18,11 @@ int main() {
 	std::chrono::time_point<std::chrono::high_resolution_clock> start_t = std::chrono::high_resolution_clock::now();
 
 	for (int j = 0; j < swimmers.size(); ++j) {
-		result.threads.emplace_back(Swimmers::pace, swimmers[j]);
+		threading.threads.emplace_back(Swimmers::pace, swimmers[j]);
 		swimmers[j]->start(start_t);
 	}
 
-	result.joinForAll();
+	threading.joinForAll();
 	for (int j = 0; j < swimmers.size(); ++j) {
 		swimmers[j]->printTime();
 	}
